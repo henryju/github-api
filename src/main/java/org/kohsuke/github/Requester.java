henryju/github-api

commit b976e0ef4ef70f26b8d75a1a847b251f8c895e62

Issue #180: don't write body if HTTP method is DELETE.

src/main/java/org/kohsuke/github/Requester.java
@@ -287,7 +287,7 @@ class Requester {
      * Set up the request parameters or POST payload.
      */
     private void buildRequest() throws IOException {
-        if (!method.equals("GET")) {
+        if (isMethodWithBody()) {
             uc.setDoOutput(true);
             uc.setRequestProperty("Content-type", contentType);
 
@@ -296,7 +296,6 @@ class Requester {
                 for (Entry e : args) {
                     json.put(e.key, e.value);
                 }
-                MAPPER.writeValue(uc.getOutputStream(), json);
             } else {
                 try {
                     byte[] bytes = new byte[32768];
@@ -309,6 +308,12 @@ class Requester {
                 }
             }
         }
+    }
+
+    private boolean isMethodWithBody() {
+        if (method.equals("GET"))       return false;
+        if (method.equals("DELETE"))    return false;
+        return true;
     }
 
     /**

commit 40b2f5c4256785ddbd758be1ee81092faff3f6cc

Fix regression introduced by b976e0ef4ef70f26b8d75a1a847b251f8c895e62

src/main/java/org/kohsuke/github/Requester.java
@@ -296,6 +296,7 @@ class Requester {
                 for (Entry e : args) {
                     json.put(e.key, e.value);
                 }
+                MAPPER.writeValue(uc.getOutputStream(), json);
             } else {
                 try {
                     byte[] bytes = new byte[32768];